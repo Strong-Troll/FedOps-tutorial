7716e4e5545543421b87362a4b8764b67d6040a0
rriakang <kangrira123@gmail.com>
2024-04-23 16:07:42 +0900

    model built delete

--- server_main.py
+++ server_main.py
@@ -25,8 +25,7 @@ def main(cfg: DictConfig) -> None:
                                                                         validation_split=cfg.dataset.validation_split, 
                                                                         batch_size=cfg.batch_size) 
     model = model.compile(optimizer='adam', loss='sparse_categorical_crossentropy', metrics=['accuracy'])
-    if not model.built:
-        model.build((None, 28, 28, 1))  # Build the model if not already built
+
     # Start fl server
     fl_server = FLServer(cfg=cfg, model=model, model_name=model_name, model_type=model_type,
                          x_val = x_val, y_val = y_val) # torch
